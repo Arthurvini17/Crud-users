cb98ee3959b48d3bc3b1605af7825d9a0e7d658f
arthur <arthurdedsec@gmail.com>
2023-09-17 22:20:11 -0300

    first

---
+++ resources/views/home.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.main')
+@section('content')
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{asset('js/app.js')}}"></script>
+<h1>teste</h1>
+
+<section id="crud">
+    <div class="container-crud">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Foto</th>
+                    <th>Name</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {{-- @foreach($dados as $item) --}}
+                <tr>
+                    {{-- <td><img src="{{ $item->foto }}" alt="Foto"></td> --}}
+                    {{-- <td>{{ $item->name }}</td> --}}
+                    {{-- <td>{{ $item->date }}</td> --}}
+                    <td>
+                        <a href="#">Editar</a>
+                        <a href="#">Excluir</a>
+                    </td>
+                </tr>
+                {{-- @endforeach --}}
+            </tbody>
+        </table>
+    </div>
+
+    <aside>
+        <nav>
+            <button>Entrar</button>
+        </nav>
+        <div class="container-search">
+            <h1>Procure por um usuario</h1>
+            <input type="search">
+        </div>
+    </aside>
+</section>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>@yield('title', 'pagina principal')</title>
+</head>
+
+<body>
+
+    <header>
+        <nav class="nav-bar">
+            <div class="logo">
+
+                <h1>User CRUD</h1>
+            </div>
+            <div class="nav-list">
+                <ul>
+                    @if (auth()->check())
+                        <li>Olá, {{ ucfirst(auth()->user()->name) }}</li>
+                    @else
+                        @guest
+                            <li><a href="{{ route('register') }}">Crie sua conta</a> </li>
+                            <li><a href="{{ route('login') }}">Entre na sua conta</a> </li>
+                        @endguest
+                    @endif
+
+                    <li> <a href="#">Contato</a></li>
+                    @auth
+                        ; <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); this.closest('form').submit();">Sair</a>
+                        </form>
+                        </li>
+                    @endauth
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    @yield('content')
+</body>
+
+</html>
